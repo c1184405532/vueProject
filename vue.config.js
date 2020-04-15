d4689a99f5d3513c66b09bfd87899c1733ca98a6
c1184405532 <1184405532@qq.com>
2020-04-15 09:00:40 +0800

    添加vw转换

--- vue.config.js
+++ vue.config.js
@@ -45,31 +45,49 @@ const postcss = px2rem({
 //     }
 //   }
 module.exports = {
+
 	css: {
 		loaderOptions: {
+			//rem转换
+			// postcss: {
+			// 	plugins: [
+			// 		require('postcss-pxtorem')({ // 把px单位换算成rem单位
+			// 			rootValue: 37.5, // 换算的基数(设计图750的根字体为32)
+			// 			selectorBlackList: ['weui', 'mu'], // 忽略转换正则匹配项
+			// 			propList: ['*']
+			// 		})
+			// 	]
+			// }
+			css: {
+				requireModuleExtension: false
+			},
 			postcss: {
+				//vw装换
 				plugins: [
-					require('postcss-pxtorem')({ // 把px单位换算成rem单位
-						rootValue: 37.5, // 换算的基数(设计图750的根字体为32)
-						selectorBlackList: ['weui', 'mu'], // 忽略转换正则匹配项
-						propList: ['*']
-					})
-				]
+					require('postcss-import'),
+					require('postcss-url'),
+					require('postcss-aspect-ratio-mini'),
+					require('postcss-write-svg')({utf8: false}),
+					require('postcss-cssnext'),
+					require('postcss-px-to-viewport')({
+						viewportWidth: 750,     // (Number) The width of the viewport.
+						viewportHeight: 1334,    // (Number) The height of the viewport.
+						unitPrecision: 3,       // (Number) The decimal numbers to allow the REM units to grow to.
+						viewportUnit: 'vw',     // (String) Expected units.
+						selectorBlackList: ['.ignore', '.hairlines','.van'],  // (Array) The selectors to ignore and leave as px.
+						minPixelValue: 1,       // (Number) Set the minimum pixel value to replace.
+						mediaQuery: false       // (Boolean) Allow px to be converted in media queries.
+					}),
+					require('postcss-viewport-units'),
+					require('cssnano')({
+						preset: "advanced",
+						autoprefixer: false,
+						"postcss-zindex": false
+					}),
+				],
 			}
-		}
+		},
+
 	},
-	// chainWebpack: config => {
-	// 	config.module
-	// 		.rule('less')
-	// 		.oneOf('vue')
-	// 		.use('postcss-pxtorem')
-			
-	// 		.options({ // 把px单位换算成rem单位
-	// 				rootValue: 37.5, // 换算的基数(设计图750的根字体为32)
-	// 				selectorBlackList: ['weui', 'mu'], // 忽略转换正则匹配项
-	// 				propList: ['*']
-	// 			})
-	// 		.end()
-	// }
 }
 
